b38e025d481c8af46eb192b72d8658da782706c9
Ben <y16chalk_b@student.trinityhigh.com>
2020-03-02 17:56:02 +0000

    Basic notice style created

--- noticeboard.php
+++ noticeboard.php
@@ -8,7 +8,13 @@
 	</head>
 
 	<body>
-		<h1> Noticeboard</h1>
+	<center>
+		<h1>Noticeboard</h1>
+		<div class="jumbotron" style="background-color: pink; width: 600px">
+			<u><h1 style="background-color: white" align="left">NOTICE TITLE</h1></u>
+			<h3 style="color: gray">Notice that this is a notice in the place of a notice.</h3>
+			<h2>The Person Who Wrote This</h2>
+		</div>
 		
 		<?php
 		
@@ -16,23 +22,24 @@
 				header("Location: index.php");
 			//Connect to Database
 			require_once('config.php');
+			echo $_SESSION['LAST_ACTIVITY'];
 
-			echo "<h1>" . $_SESSION['current_user'] . "</h1>";
 			$allNotices = $link->query("SELECT * FROM notices WHERE public = '1'")->fetch_all(MYSQLI_ASSOC);
-			
-			//$myNotices = $link->query("SELECT * FROM notices WHERE uploader_id ='" . $_SESSION['current_user'] . "'")->fetch_all(MYSQLI_ASSOC);
-			
+		
+			$jumbotronOpen = "<div class=\"jumbotron\" style=\"width: 600px\">";
 			
 			//This prints out the title of the first notice 
 			//[0] refers to the first notice
 			//['title'] refers to the title field
+
 			foreach($allNotices as $notice){
-				echo $notice['title'] . "<br>";
+				// echo $jumbotronOpen . $notice['title'] . "<br>";
+
 			}
 			
 			
 		
 		?>
-	
+	</center>
 	</body>
 </html>
